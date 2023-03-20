018894b2c03df7f4c0e768324bd4c5fefbe5adf7
Irfan Hanjra <ihanjra@gmail.com>
2023-03-20 13:57:01 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(data) {
+  finder(data.first, data.last)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
 
 /**
  * logTwoResults()
@@ -22,7 +30,17 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(data1, data2) {
+  for (let argument of arguments) {
+    finder(argument.first, argument.last)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +53,17 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(...data) {
+  data.forEach((item) => {
+    finder(item.first, item.last)
+      .then((result) => {
+        console.log(result[0].city)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+}
 
 // Do not change any of the code below this line.
 module.exports = {
