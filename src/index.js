8b4118de7ae69009fabf5ee753d28ed13816394b
Michelle Harley <chell.hx@gmail.com>
2023-03-20 18:26:28 -0400

    refactor promise lab

--- src/index.js
+++ src/index.js
@@ -30,22 +30,16 @@ function logResult(person) {
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults(person1, person2) {
-  finder(person1.first, person1.last)
-    .then((person1) => {
-      console.log(person1);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-  finder(person2.first, person2.last)
-    .then((person2) => {
-      console.log(person2);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+function logTwoResults(...person) {
+  person.forEach((persons) => {
+    let result = finder(persons.first, persons.last)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 }
 
 /**
@@ -59,28 +53,16 @@ function logTwoResults(person1, person2) {
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities(person1, person2, person3) {
-  finder(person1.first, person1.last)
-    .then((person1) => {
-      console.log(person1[0].city);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  finder(person2.first, person2.last)
-    .then((person2) => {
-      console.log(person2[0].city);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  finder(person3.first, person3.last)
-    .then((person3) => {
-      console.log(person3[0].city);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+function logThreeResultsCities(...person) {
+  person.forEach((persons) => {
+    let result = finder(persons.first, persons.last)
+      .then((result) => {
+        console.log(result[0].city);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 }
 
 // Do not change any of the code below this line.
