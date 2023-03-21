b1c7181ee66562055eb7acb900cbafcf81c36fc5
Karma Ghale <karmaghale@karmas-air-2.mynetworksettings.com>
2023-03-21 00:11:59 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(data) {
+  finder(data.first, data.last)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    }); 
+}
 
 /**
  * logTwoResults()
@@ -22,12 +30,25 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(...data) {
+
+  for(let i=0; i<data.length;i++){
+
+    finder(data[i].first, data[i].last)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    }); 
+  }
+}
 
 /**
  * logThreeResultsCities()
  * ========================
- * Logs out just the cities from all three people given to the function. You may assume that each search result will only return a single person.
+ * Logs out just the cities from all three people given to the function. You may assume that each search
+ *  result will only return a single person.
  *
  * @param {Object} person1 - A person's names.
  * @param {Object} person1.first - The person's first name.
@@ -35,7 +56,22 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(...data) {
+  for(let i=0; i<data.length;i++){
+
+    finder(data[i].first, data[i].last)
+    .then((result) => {
+      result.forEach(element => {
+        console.log(element.city)
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    }); 
+  }
+}
+
+
 
 // Do not change any of the code below this line.
 module.exports = {
