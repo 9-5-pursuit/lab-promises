243b7712d1d47cc5983fc03288cf803d4729eecd
Erika Medina <erikamedina@pursuit.org>
2023-04-07 17:58:12 -0400

    promises

--- src/index.js
+++ src/index.js
@@ -10,18 +10,16 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult(data) {
-    
-    finder(arguments.first, arguments.last)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+async function logResult(person) {
+  try {
+    const result = await finder(person.first, person.last);
+    console.log(result);
+  } catch (error) {
+    console.log(error);
   }
+}
 
-
+  
 
 /**
  * logTwoResults()
@@ -33,18 +31,12 @@ function logResult(data) {
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {
-  for (let i = 0; i < arguments.length; i++) {
-    
-    finder(arguments[i].first, arguments[i].last)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+
+async function logTwoResults(person1, person2) {
+  await logResult(person1);
+  await logResult(person2);
 }
+
 
 
 /**
@@ -58,16 +50,19 @@ function logTwoResults() {
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities(...data) {
-  data.forEach((item) => {
-    finder(item.first, item.last)
-      .then((result) => {
-        console.log(result[0].city);
-      })
-      .catch((error) => { 
-        console.log(error);
-      }); 
-  }); 
+async function logThreeResultsCities(person1, person2, person3) {
+  try {
+    const result1 = await finder(person1.first, person1.last);
+    console.log(result1[0].city);
+
+    const result2 = await finder(person2.first, person2.last);
+    console.log(result2[0].city);
+
+    const result3 = await finder(person3.first, person3.last);
+    console.log(result3[0].city);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // Do not change any of the code below this line.
